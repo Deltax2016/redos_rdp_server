ac5c1037eca77cfcc36a700b6355360182c76acc
Deltax2016 <deltaxussr@gmail.com>
2021-06-19 19:56:12 +0500

    testing sockets

--- java.py
+++ java.py
@@ -10,13 +10,13 @@ while True:
     conn, addr = soc.accept()
     print("Got connection from",addr)
     length_of_message = int.from_bytes(conn.recv(2), byteorder='big')
-    msg = conn.recv(length_of_message)
+    msg = conn.recv(length_of_message).encode("UTF-8")
     print(msg)
     print(length_of_message)
 
     # Note the corrected indentation below
-    if "Hello"in msg:
-        message_to_send = "bye".encode("UTF-8")
+    if "Hello" in msg:
+        message_to_send = "bye"
         conn.send(len(message_to_send).to_bytes(2, byteorder='big'))
         conn.send(message_to_send)
     else:
