c97c1ab3723c7125b9f4d2f6c0cde82504ee8d3f
Deltax2016 <deltaxussr@gmail.com>
2021-06-20 01:54:31 +0500

    new sev

--- sio.py
+++ sio.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room, leave_room,
-close_room, rooms, disconnect
+from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
 
 session = {}
 
